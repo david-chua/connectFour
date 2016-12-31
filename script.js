ec3a91d2dee02981ad49d079439f467b7e5be199
dp_chua <dp_chua@hotmail.com>
2016-12-31 01:11:22 -0500

    created board and functions

--- script.js
+++ script.js
@@ -4,47 +4,161 @@ ConnectFourArray = function() {
 
 // multi-dimensional array model for a connect four game. A board is generally 7 columns with 6 rows.
 // 0 will represent spots that has not been used by players before. A red or black will represent the players.
-const GameEngine = {}
- var player1 = {
-   locationX = 0,
-   locationY = 0,
+const GameEngine = {
+  gameover: "false",
+  player1: "Red",
+  board:
+    [
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+    ],
+
+ player1: {
+   locationX: 0,
+   locationY: 0,
    height: 50,
    width: 50,
-   backgroundColor: "Red",
+   bgColor: "Red",
    gamePieceX: [],
    gamePieceY: [],
- };
- var player2 = {
-   locationX = 0,
-   locationY = 0,
+ },
+ player2: {
+   locationX: 0,
+   locationY: 0,
    height: 50,
    width: 50,
-   backgroundColor: "Blue",
+   bgColor: "Blue",
    gamePieceX: [],
    gamePieceY: [],
- };
+ },
+  // var Row = board.length;
+  // var Column = board[0].length;
+  resetGame: function(){
+    this.board =   [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+      ];
+    var currentPlayer = player1;
+    this.gameOver = "false";
+  },
 
- var board =
-  [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-  ];
+  toggleCurrentPlayer: function(){
+    if (this.currentPlayer === "player1") {
+      this.currentPlayer = "player2";
+    } else {
+      this.currentPlayer = "player1";
+    }
+  },
 
-  var Row = board.length;
-  var column = pattern[0].length;
+render: function(){
+  document.getElementById('board').innerHTML= GameEngine.board;
+  for (i=0; i<GameEngine.board.length;i++) {
+    var row = document.getElementById('row');
+    for (j-0; j <GameEngine.board[0].length; j++) {
+      var column = document.getElementById('column')
+    }
+  }
+},
 
- var playerA = new Object(player1);
- var playerb = new Object(player2);
+printBoard: function(){
+  for(var i=0; i< GameEngine.board.length; i++)
+   {
+     var output="";
+      for (var j=0; j <GameEngine.board[0].length; j++)
+    {
+      if (board[i][j] === 1) {
+        output +=  "Red";
+      } else if (board[i][j] === 2) {
+          output +=  "Blue";
+      } else {
+        output += "white";
+      }
+    }
+    return output;
+    }
+},
+checkForVictory: function(){
+  //vertical victory
+  for (var i = 0; i < 3; i++)
+  {
+    for (var j = 0; j<7; j++)
+    {
+      if (board[i][j] != 0 &&
+      board[i][j] === board[i+1][j] &&
+      board[i][j] === board[i+2][j] &&
+      board[i][j] === board[i+3][j]){
+        return board[i][j] + "is the winner";
+      }
+    }
+  }
+  //horizontal victory
+  for (var i = 0; i < 6; i++){
+    for (var j = 0; j<4; j++){
+      if (board[i][j] != 0 &&
+      board[i][j] === board[i][j+1] &&
+      board[i][j] === board[i][j+2] &&
+      board[i][j] === board[i][j+3]){
+        return board[i][j] + "is the winner";
+      }
+    }
+  }
+  //diagonal victory - right and down
+  for (var i = 0; i < 3; i++){
+    for (var j = 0; j<4; j++){
+      if (board[i][j] != 0 &&
+      board[i][j] === board[i+1][j+1] &&
+      board[i][j] === board[i+2][j+2] &&
+      board[i][j] === board[i+3][j+3]){
+        return board[i][j] + "is the winner";
+      }
+    }
+  }
+  //diagonal victory - right and up
+  for (var i = 3; i >=0; i--){
+    for (var j = 0; j<7; j++){
+      if (board[i][j] != 0 &&
+      board[i][j] === board[i-1][j+1] &&
+      board[i][j] === board[i-2][j+2] &&
+      board[i][j] === board[i-3][j+3]){
+        return board[i][j] + "is the winner";
+      }
+    }
+  }
+},
+
+}
+window.printBoard = printBoard;
+}
 
 
-   drawmsg = "This game is a draw!",
-   winmsg = "The winner is: ",
-   currentPlayermsg = "Current Player is: ",
- }
+
+//       var piece = document.createElement("div");
+//       piece.setAttribute("class","piece");
+//       stage.appendChild(piece);
+//       if(board[i][j]===3)
+//       {
+//           cell.style.backgroundColor="white";
+//       }
+// //player 1 is 1 and player 2 is 2.
+//       if(board[i][j]===1)
+//       {
+//           piece.style.backgroundColor = player1.bgColor;
+//       }
+//       if(pattern[i]===2)
+//       {
+//         piece.style.backgroundColor = player2.bgColor;
+//       }
+//     piece.style.top = i * pieceSize + "px";
+//     piece.style.top = j * pieceSize + "px";
 
 
-document.addEventListener("click", moving.)
+// Top Level App Control //
+// document.addEventListener("click", moving.)
